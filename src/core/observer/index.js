8edae36ae93de4e9564be9617afbedd2dbabf2a6
贾永枫 <wb-jyf922196@antgroup.com>
2021-04-15 15:59:25 +0800

    update

--- src/core/observer/index.js
+++ src/core/observer/index.js
@@ -213,18 +213,29 @@ export function set (target: Array<any> | Object, key: any, val: any): any {
   if (process.env.NODE_ENV !== 'production' &&
     (isUndef(target) || isPrimitive(target))
   ) {
-    warn(`Cannot set reactive property on undefined, null, or primitive value: ${(target: any)}`)
+    warn(`Cannot set reactive property on undefined, null, or primitive value: ${(target)}`)
   }
+  // 判断是否是数组，并进行合理性校验
+  // export function isValidArrayIndex (val: any): boolean {
+  //   const n = parseFloat(String(val))
+  //   return n >= 0 && Math.floor(n) === n && isFinite(val)
+  // }
+  // isFinite() 函数用于检查其参数是否是无穷大，也可以理解为是否为一个有限数值（finite number）。
+  // 提示： 如果参数是 NaN，正无穷大或者负无穷大，会返回 false，其他返回 true。
   if (Array.isArray(target) && isValidArrayIndex(key)) {
+    // 安全手段，如果key很大，就扩充target
     target.length = Math.max(target.length, key)
+    // 替换操作，此时splice已替换过，会进行通知
     target.splice(key, 1, val)
     return val
   }
+  // 如果key在target，且不在原型
   if (key in target && !(key in Object.prototype)) {
+    // 此时在target，则已经是个响应式的了，直接赋值即可
     target[key] = val
     return val
   }
-  const ob = (target: any).__ob__
+  const ob = (target).__ob__
   if (target._isVue || (ob && ob.vmCount)) {
     process.env.NODE_ENV !== 'production' && warn(
       'Avoid adding reactive properties to a Vue instance or its root $data ' +
@@ -232,11 +243,14 @@ export function set (target: Array<any> | Object, key: any, val: any): any {
     )
     return val
   }
+  // 如果本身不是一个响应式对象，就直接赋值，也不会做响应式处理
   if (!ob) {
     target[key] = val
     return val
   }
+  // 做响应式处理
   defineReactive(ob.value, key, val)
+  // 通知
   ob.dep.notify()
   return val
 }
@@ -248,13 +262,15 @@ export function del (target: Array<any> | Object, key: any) {
   if (process.env.NODE_ENV !== 'production' &&
     (isUndef(target) || isPrimitive(target))
   ) {
-    warn(`Cannot delete reactive property on undefined, null, or primitive value: ${(target: any)}`)
+    warn(`Cannot delete reactive property on undefined, null, or primitive value: ${(target)}`)
   }
+  // 和set一样，数组的检测
   if (Array.isArray(target) && isValidArrayIndex(key)) {
+    // 删除
     target.splice(key, 1)
     return
   }
-  const ob = (target: any).__ob__
+  const ob = (target).__ob__
   if (target._isVue || (ob && ob.vmCount)) {
     process.env.NODE_ENV !== 'production' && warn(
       'Avoid deleting properties on a Vue instance or its root $data ' +
@@ -262,13 +278,16 @@ export function del (target: Array<any> | Object, key: any) {
     )
     return
   }
+  // 如果没有这个key就直接跳出
   if (!hasOwn(target, key)) {
     return
   }
+  // 有的话就删除
   delete target[key]
   if (!ob) {
     return
   }
+  // 通知
   ob.dep.notify()
 }
 
